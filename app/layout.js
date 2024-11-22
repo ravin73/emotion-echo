2af27620cc6a7609f602a77f6c02bb8801535e04
Ravin Yadav <yravin72@gmail.com>
2024-11-22 13:57:38 +0530

    make resposive all pages and add favicon also

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Link from "next/link";
 import { AuthProvider } from "@/context/AuthContext";
 import Head from "./head";
+import Logout from "@/components/Logout";
 
 
 
@@ -21,16 +22,16 @@ export default function RootLayout({ children }) {
       <Link href={'/'}>
         <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>AuaTrack</h1>
       </Link>
-      
+      <Logout/>
     </header>
   )
 
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
-      <Link href={'https://youtu.be/lkjrUW8fI40'} target="_blank" className="">
+     
         <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500 ' + fugaz.className}>Built by Aura-aplus </p>
         
-      </Link>
+     
       <h3 className="pt-[24px] text-center text-[14px] text-slate-800 font-medium">&copy; 2024 AuraTrack. All rights reserved.</h3>
     </footer>
   )
--- components/Hero.js
+++ components/Hero.js
@@ -4,25 +4,18 @@ import { Fugaz_One } from "next/font/google";
 import Button from "./Button";
 import Calendar from "./Calendar";
 import Link from "next/link";
+import CallToAction from "./CallToAction";
 
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 export default function Hero() {
     return (
-        <div className="py-4  md:py-10 flex flex-col gap-4 sm:gap-10  md:gap-10">
+        <div className="py-4  md:py-10 flex flex-col gap-5 sm:gap-10  md:gap-10">
             <h1 className={'text-5xl sm:text-text-6xl md:text-7xl text-center ' + fugaz.className}><span className='textGradient'> AuraTracker</span> helps you track your<span className="textGradient"> daily </span> Aura!</h1>
             <p className="text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px] ">Create you aura 🗿 and see How you feel<span className="font-semibold"> every-day of the year</span></p>
-            <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
-                <Link href={'/dashboard'}>
-                    <Button text="Sign Up" />
-                </Link>
-                <Link href={'/dashboard'}>
-                    <Button text="Login" dark />
-                </Link>
-
-            </div>
-            <Calendar />
+            <CallToAction/>
+            <Calendar demo />
         </div>
     )
 }
